49a613915cc4572a2e1815f55b4e28f88cb97a04
Sahajdeep Singh <impulseVD@hotmail.com>
2023-02-15 11:41:39 -0500

    added the customer class with the constructor, be sure to implement mySQL for sign-up

---
+++ Customer.java
@@ -0,0 +1,18 @@
+
+
+public class Customer {
+
+    private String email;
+    private String user_name;
+    private String pass_word;
+
+    public Customer (String email, String user_name, String pass_word) {
+        this.email = email;
+        this.user_name = user_name;
+        this.pass_word = pass_word;
+    }
+
+    private void customer_sign_up (String email, String user_name, String pass_word) {
+        ; // make use of mysql database
+    }
+}
